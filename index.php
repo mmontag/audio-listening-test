da15e050573d0550de6075669b1c204f0ada3d16
Matt Montag <montag@spotify.com>
2013-02-10 04:48:11 -0800

    Test page items populated dynamically

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require("audioswitcher.inc.php");
+
 ?>
 <!doctype html>
 <html>
@@ -9,58 +11,52 @@
 	<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.1/underscore-min.js"></script>
 	<script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.1/backbone-min.js"></script>
 	<script type="text/javascript" src="audioswitcher.js"></script>
+  <script>
+      var audioSwitcher = [];
+  </script>
 	<link rel="stylesheet" href="audiotest.css">
 </head>
 <body>
 	<h1>Audio Test</h1>
-
+  <form action="submit.php" method="post">
+  <input type="hidden" name="token" value="<?=$token?>"/>
 	<ol>
-		<li>
-            <div class="listitem">
+    <?php
+    foreach($filemap as $index => $fileset) {
+    ?>
+        <li>
+            <div class="listitem" id="audioswitcher<?=$index?>">
                 <div class="col1">
-                    <button class="switch" id="a">A</button>
+                    <button type="button" class="switch a">A</button>
                     <div class="chooser">
-                        <input type="radio" id="chooser_0_0" name="audio_0" value="0">
-                        <label for="chooser_0_0">A is Watermarked</label>
+                        <input type="radio" id="chooser_<?=$index?>_0" name="audio_<?=$index?>" value="0">
+                        <label for="chooser_<?=$index?>_0">A is Watermarked</label>
                     </div>
                 </div>
                 <div class="col2">
-                    <button class="toggle" id="toggle">Toggle &rarr;</button>
-                    <button class="toggle" id="rewind">Rewind &#8634; 3 sec</button><br>
-                    <button class="toggle" id="toggleAndRewind">Toggle and Rewind &#8634; 3 sec</button>
-                    <audio id="audio_0_0" controls="true" src="audio/file_1.mp3"></audio>
-                    <audio id="audio_0_1" controls="true" src="audio/file_2.mp3" style="display: none"></audio>
+                    <button type="button" class="toggle">Toggle &harr;</button>
+                    <button type="button" class="rewind">Rewind &#8634; 3 sec</button><br>
+                    <button type="button" class="toggleAndRewind">Toggle and Rewind &#8634; 3 sec</button>
+                    <audio id="audio_<?=$index?>_0" controls="true" src="getAudio.php?token=<?=$token?>&file=<?=$index?>,0"></audio>
+                    <audio id="audio_<?=$index?>_1" controls="true" src="getAudio.php?token=<?=$token?>&file=<?=$index?>,1" style="display: none"></audio>
                 </div>
                 <div class="col3">
-                    <button class="switch" id="b">B</button>
+                    <button type="button" class="switch b">B</button>
                     <div class="chooser">
-                        <input type="radio" id="chooser_0_1" name="audio_0" value="1">
-                        <label for="chooser_0_1">B is Watermarked</label>
+                        <input type="radio" id="chooser_<?=$index?>_1" name="audio_<?=$index?>" value="1">
+                        <label for="chooser_<?=$index?>_1">B is Watermarked</label>
                     </div>
                 </div>
             </div>
-		</li>
+        </li>
+        <script>
+          audioSwitcher[<?=$index?>] = new AudioSwitcher(document.getElementById('audioswitcher<?=$index?>'));
+        </script>
+    <?php
+    }
+    ?>
 	</ol>
-    <script>
-        var audioSwitcher1 = new AudioSwitcher(document.getElementById('audio_0_0'), document.getElementById('audio_0_1'));
-        $('#a').bind('click', function() {
-            audioSwitcher1.switchToAndPlay(0);
-        });
-        $('#b').bind('click', function() {
-            audioSwitcher1.switchToAndPlay(1);
-        });
-
-        $('#toggle').bind('click', function() {
-            audioSwitcher1.cycle();
-        });
-        $('#rewind').bind('click', function() {
-            audioSwitcher1.rewind(3);
-        });
-        $('#toggleAndRewind').bind('click', function() {
-            audioSwitcher1.rewind(3);
-            audioSwitcher1.cycle();
-        });
-    </script>
+  </form>
 <!-- Templates -->
 <script type="text/template" id="tpl-switcher">
   <li>
