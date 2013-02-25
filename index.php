2d05d747dbfd39884b072fc718675f624e2fe6c6
Matt Montag <montag@spotify.com>
2013-02-24 16:23:36 -0800

    Google Analytics

--- index.php
+++ index.php
@@ -223,5 +223,19 @@ if(isset($_POST['token'])) {
   <?
 }
 ?>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25089589-1']);
+  _gaq.push(['_setCampNameKey', 'Listening Test']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
